181c27df5c7d09e1199837d6f45530add1604ced
Hande Eylül Memikoğlu <126772723+handememikoglu@users.noreply.github.com>
2025-06-29 11:23:14 +0000

    ssr control removed

--- src/app/components/ThemeSwitch.jsx
+++ src/app/components/ThemeSwitch.jsx
@@ -5,16 +5,7 @@ import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 
 export default function ThemeSwitch() {
-  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return null;
-  }
 
   return (
     <button
